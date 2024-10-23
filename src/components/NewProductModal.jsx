b28d411b786b6f87da020cbbac0bfe1b093f3474
Izzat <izzatmuz@gmail.com>
2024-10-23 14:47:00 +0800

    added function to store image

--- src/components/NewProductModal.jsx
+++ src/components/NewProductModal.jsx
@@ -2,40 +2,61 @@ import axios from 'axios'
 import { useState } from 'react'
 import { Button, Form, Modal } from 'react-bootstrap'
 import { BASE_URL } from './BaseUrl'
+import { storage } from '../firebase'
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
 
 export default function NewProductModal({ show, handleClose }) {
     const [productName, setProductName] = useState("")
-    // const [productImage, setProductImage] = useState(null)
-    const [productImage, setProductImage] = useState("")
+    // const [productImage, setProductImage] = useState("")
+    const [productImage, setProductImage] = useState(null)
     const [productSpecs, setProductSpecs] = useState("")
     const [productPros, setProductPros] = useState("")
     const [productCons, setProductCons] = useState("")
     const [productLink, setProductLink] = useState("")
     const [productVideo, setProductVideo] = useState("")
 
-    // to create function for image storage
-    const handleSave = () => {
-        const data = {
-            name: productName,
-            image_url: productImage,
-            specification: productSpecs,
-            pros: productPros,
-            cons: productCons,
-            referral_link: productLink,
-            video_url: productVideo,
-            created_at: new Date().toISOString(),
-        };
+    const handleSave = async () => {
+        try {
+            // function for image storage
+            let imageUrl = ""
+            console.log(productImage)
+            if (productImage !== null) {
+                const imageRef = ref(storage, `productImages/${productImage.name}`)
+                const response = await uploadBytes(imageRef, productImage)
+                imageUrl = await getDownloadURL(response.ref)
+            }
+            console.log(imageUrl)
 
-        axios
-            .post(`${BASE_URL}/products`, data)
-            .then((response) => {
-                console.log("Success:", response.data)
-                handleClose()
-            })
-            .catch((error) => {
-                console.error("Error", error)
-            })
+            // prepare data for post request
+            const data = {
+                name: productName,
+                image_url: imageUrl,
+                specification: productSpecs,
+                pros: productPros,
+                cons: productCons,
+                referral_link: productLink,
+                video_url: productVideo,
+                created_at: new Date().toISOString(),
+            };
+            console.log(data)
+
+            // post request to add product
+            axios
+                .post(`${BASE_URL}/products`, data)
+                .then((response) => {
+                    console.log("Success:", response.data)
+                    handleClose()
+                })
+                .catch((error) => {
+                    console.error("Error", error)
+                })
+        } catch (error) {
+            console.error(error)
+            throw error
+        }
     }
+
+    const handleFileChange = (e) => { setProductImage(e.target.files[0]) }
 
     return (
         <>
@@ -52,9 +73,10 @@ export default function NewProductModal({ show, handleClose }) {
                             />
                             <Form.Control
                                 placeholder="Upload product image"
-                                onChange={(e) => setProductImage(e.target.value)}
+                                onChange={handleFileChange}
                                 className='mb-2'
                                 required
+                                type='file'
                             />
                             <Form.Control
                                 placeholder="Enter product specification"
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
@@ -18,3 +19,4 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+export const storage = getStorage(app);
